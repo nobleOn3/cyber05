6e5c8ae0065d110d912fe0981c69cfccba5b7993
nobleOn3 <nob16001@mybyui.edu>
2020-02-06 11:28:12 -0700

    Added some baseline code for the group to work with, like a freamework for a menu, some function stubs, and a comment line or two.

--- proj05.cpp
+++ proj05.cpp
@@ -1,1 +1,60 @@
-/*Programming Start*/
+/*************************************************************
+ * Lab 05 code
+ * This file contains our solutions to the problems of 
+ * Homographs and Canacolization.
+ * @auth: Jessen Noble, ..., ..., ..., ..., ...
+*************************************************************/
+include <iostream>
+include <fstream>
+include <string>
+
+
+std::string R(std::string e)
+{
+
+}
+
+std::string C(std::string e)
+{
+
+}
+
+bool H(std::string e1, std::string e2)
+{
+    return C(e1) == C(e2);
+}
+
+
+
+int main()
+{
+    cout << "Welcome to Lab 05\n"
+        << "To access the --- test type 0\n"
+        << "To access the --- test, type 1\n"
+        << "To access the --- test, type 2\n";
+    while(!q)
+    {
+        cout<< "What is your command: ";
+        cin >> cmd;
+        switch(cmd)
+        {
+            case 0:
+            {
+
+            }
+            case 1:
+            {
+
+            }
+            case 2:
+            {
+
+            }
+            case 3:
+            {
+
+            }
+        }
+    }
+
+}
